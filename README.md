385e540182492971281c2ab9002ab8bb3e59061d
Christina Smithers <christina.smithers@gmail.com>
2017-09-28 12:09:01 -0700

    This sommit has the counts of each character.  TODO: slice the text where the underscore is to reveal the secret word and condense the letter

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 "# Cipher" 
+
+### Summary
+This is a simple Python application that will unscamble a cipher text.
+
+### Requirements
+You will need Python 3 on your system which can be found [here](python.org).  Make sure you are using Python 3 instead of 2 since the syntax differs enough to make a difference here.
+
+### To run
+Clone this repo using the GitHub GUI or you can do a `git clone` locally on your system if you are comfortable with terminal commands.
+
+After you clone this, navigate to the directory you have and use the python command of `python unscramble_text.py`.
--- unscramble_text.py
+++ unscramble_text.py
@@ -3,12 +3,15 @@
 # Using the counter from the Python collections since it goes through setting up the counter for you
 from collections import Counter
 
-# First let's open the text file and get a block of scrambled text
+# First let 's open the text file and get a block of scrambled text
 with open('scrambled_text.txt') as scrambled_text_from_file:
-    scrambled_text_from_file = scrambled_text_from_file.read()
-    print ("Here is the original text:")
-    print (scrambled_text_from_file)
+  scrambled_text_from_file = scrambled_text_from_file.read()
+print("Here is the original text:")
+print(scrambled_text_from_file)
 
-    # Now we sort it out and count the number of times a character by using a dictionary
-    print ("Here is a count on how often a character appears: ")
-    print (sorted(Counter(scrambled_text_from_file.replace('_', '')).most_common()))
+# Now we sort it out and count the number of times a character shows up
+print("Here is a count on how often a character appears: ")
+counted_text = Counter(scrambled_text_from_file)
+print (counted_text)
+
+# Final result spells keyboarding
